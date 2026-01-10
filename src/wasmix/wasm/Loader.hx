c8d871a318b22c718a2b846317c2743e933afd9d
Juraj Kirchheim <back2dos@gmail.com>
2026-01-10 18:02:07 +0100

    Leaner load code.

--- src/wasmix/wasm/Loader.hx
+++ src/wasmix/wasm/Loader.hx
@@ -1,21 +1,34 @@
 package wasmix.wasm;
 
+import js.lib.*;
+import haxe.io.Bytes;
+
 class Loader {
-  overload static public extern inline function load(base64:String, imports:{}) {
+  overload static public extern inline function load(base64:String, imports:{}) 
     return load(haxe.crypto.Base64.decode(base64), imports);// TODO: optimize
-  }
 
-  overload static public extern inline function load(buf:haxe.io.Bytes, imports:{}) {
-    return loadBuffer(buf.getData().slice(0, buf.length), imports);
-  }
+  overload static public extern inline function load(bytes:Bytes, imports:{}) 
+    return loadBuffer(bufOfBytes(bytes), imports);
 
-  overload static public extern inline function load(buf:js.lib.ArrayBuffer, imports:{}) {
+  overload static public extern inline function load(buf:ArrayBuffer, imports:{}) 
     return loadBuffer(buf, imports);
+
+  static function bufOfBytes(bytes:Bytes):ArrayBuffer
+    return bytes.getData().slice(0, bytes.length);
+
+  static function decode(base64:String):ArrayBuffer {
+    #if nodejs
+      return js.node.Buffer.from(base64, 'base64').buffer;
+    #else
+      final bin = js.Browser.window.atob(base64);
+      final bytes = new Uint8Array(bin.length);
+      
+      for (i in 0...bin.length) bytes[i] = bin.charCodeAt(i) & 0xFF;
+      
+      return bytes.buffer;
+    #end
   }
 
-  static function loadBuffer(buf:js.lib.ArrayBuffer, imports:{}) {
-    return js.lib.WebAssembly.instantiate(buf, imports).then(function(result) {
-      return result.instance;
-    });
-  }
+  static function loadBuffer(buf:ArrayBuffer, imports:{}) 
+    return js.lib.WebAssembly.instantiate(buf, imports).then(r -> r.instance);
 }
